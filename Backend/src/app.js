313fbc3bd3d3f3b7bfd19575cb2c47d4e792a238
Aditya218-maker <aditya777anand@gmail.com>
2026-06-14 17:51:48 +0530

    feat: update button colors and enhance CORS configuration for multiple origins

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,7 @@ app.use(cookieParser())
 app.use(morgan("dev"))
 app.use(cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:5174"],
     methods: [ "GET", "POST", "PUT", "DELETE" ],
 }))
 
