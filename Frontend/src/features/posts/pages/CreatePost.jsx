48d8e111fba0f0841058da94764d08d42e56a610
Aditya218-maker <aditya777anand@gmail.com>
2026-06-21 20:26:13 +0530

    style: update styling for post image label and navigation bar

--- Frontend/src/features/posts/pages/CreatePost.jsx
+++ Frontend/src/features/posts/pages/CreatePost.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import { useState, useRef } from 'react'
 import "../style/createpost.scss"
 import { usePost } from '../hook/usePost'
 import { useNavigate } from 'react-router'
